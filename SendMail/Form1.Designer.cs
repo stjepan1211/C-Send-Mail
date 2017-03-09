81b464c02e90d38727ce2278b0c8c2f0681f8a0e
Stjepan Baricevic <Stjepan Baricevic>
2017-03-09 01:21:46 +0100

    application sends mail from tournamentapp1@gmail.com

---
+++ SendMail/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SendMail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SendMail = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SendMail
+            // 
+            this.SendMail.Location = new System.Drawing.Point(12, 39);
+            this.SendMail.Name = "SendMail";
+            this.SendMail.Size = new System.Drawing.Size(75, 23);
+            this.SendMail.TabIndex = 0;
+            this.SendMail.Text = "Send";
+            this.SendMail.UseVisualStyleBackColor = true;
+            this.SendMail.Click += new System.EventHandler(this.SendMail_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.SendMail);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SendMail;
+    }
+}
+
---
+++ SendMail/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SendMail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMail_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("saljem1");
+                Send();
+                MessageBox.Show("saljem2");
+            }
+            catch(Exception exception)
+            {
+                //MessageBox.Show(exception.InnerException.ToString());
+            }
+        }
+
+        protected void Send()
+        {
+            // Gmail Address from where you send the mail
+            var fromAddress = "tournamentapp1@gmail.com"; //TODO: add your email
+            // any address where the email will be sending
+            var toAddress ="stjepanbaricevic1211@gmail.cm";
+            //Password of your gmail address
+            //read password from text file
+            string fromPassword = System.IO.File.ReadAllText(@"E:\Visual studio projekti\SendMail\password.txt");
+            // Passing the values and make a email formate to display
+            string subject = "Hello from Tournaments application";
+            string body = "From: " + "Stjepan Baricevic" + "\n";
+            body += "Email: " + "Hi, log in and check application features." + "\n";
+            body += "Questions: \n" + "stjepanbaricevic1211@gmail.com" + "\n";
+            // smtp settings
+            var smtp = new SmtpClient();
+            {
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                smtp.Credentials = new NetworkCredential(fromAddress, fromPassword);
+                smtp.Timeout = 20000;
+            }
+            // Passing values to smtp object
+            smtp.Send(fromAddress, toAddress, subject, body);
+        }
+    }
+}
